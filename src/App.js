7083e3577bfba52b0be54b4b6acadd4232724d7b
mikesuchor <mikesuchor@gmail.com>
2019-02-05 21:59:21 -0700

    Imported components into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Health from 'Health';
+import Level from 'Level';
+import Weapon from 'Weapon';
 
 class App extends Component {
   render() {
