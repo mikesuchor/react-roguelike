5a9ea08f6f2c62ddaabe29443715a0e4aae1bf35
mikesuchor <mikesuchor@gmail.com>
2019-02-09 11:55:07 -0800

    Fixed limports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Health from 'Health';
-import Level from 'Level';
-import Weapon from 'Weapon';
+import Health from '../Health';
+import Level from '../Level';
+import Weapon from '../Weapon';
 
 class App extends Component {
   render() {
