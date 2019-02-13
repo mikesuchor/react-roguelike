decb62f870542d8b7ef7a9360376418dd4c5e2a5
mikesuchor <mikesuchor@gmail.com>
2019-02-12 23:32:17 -0700

    Preparing Map component for game map/grid

---
+++ src/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Map.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Map;
